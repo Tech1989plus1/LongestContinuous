e9533b17e34d0d9b99724797fcc6d0281f7ec6a6
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-08-23 14:54:33 -0700

    created logic

---
+++ LongestContinuous.js
@@ -0,0 +1,28 @@
+var longestContinuous = (arr) => { 
+  if (!Array.isArray(arr) || arr[0] === 'undefined' || !Number(arr[0])) {
+    return 0;
+  }
+
+  var longest = 0;
+
+  const recursive = (val, i, continuous, arr, length) => {
+    if (i > length) {return 0;}
+
+    if (Number(arr[i]) === NaN) {return 0;}
+
+    if (val === arr[i]) {
+        recursive(val, i+1, continuous+1, arr, arr.length);   
+    }
+
+    if (continuous > longest) {
+        longest = continuous;
+    }
+
+    recursive(arr[i], i+1, 1, arr, arr.length);
+    
+  }
+  
+  recursive(arr[0], 1, 1, arr, arr.length);
+
+  return longest;
+}
